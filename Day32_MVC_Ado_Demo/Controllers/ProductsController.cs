0065967925f165252a7d14d54525264e25a1836b
Ankit-Dwivedi-code <152896806+Ankit-Dwivedi-code@users.noreply.github.com>
2025-08-25 17:38:01 +0530

    A MVC application connected with Database

---
+++ Day32_MVC_Ado_Demo/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+using Day32_MVC_Ado_Demo.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace Day32_MVC_Ado_Demo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ProductsRepository _productsRepository;
+
+        public ProductsController(ProductsRepository productsRepository)
+        {
+            _productsRepository = productsRepository;
+        }
+
+        // Action methods go here
+        // Example: Get all products
+        public IActionResult Index()
+        {
+            var products = _productsRepository.GetProducts();
+            return View(products);
+        }
+
+        // Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string name, decimal price)
+        {
+            if (ModelState.IsValid && !string.IsNullOrEmpty(name) && price > 0)
+            {
+                var productId = _productsRepository.InsertProduct(name, price);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+    }
+}
---
+++ Day32_MVC_Ado_Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Day32_MVC_Ado_Demo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
